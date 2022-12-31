9d18efcbd49460c09c48bbe1d87886ef49940c49
Michael Morten Sonne <49366751+michaelmsonne@users.noreply.github.com>
2022-12-31 14:01:45 +0100

    Update rename/cleanup script for build name of .exe

--- AzureDevOpsBackup/RenameOutput.ps1
+++ AzureDevOpsBackup/RenameOutput.ps1
@@ -9,13 +9,11 @@ else {
     #PowerShell Create directory if not exists
     New-Item $FolderName -ItemType Directory
 }
+#Delete old .exe file there not need to be used anymore 2 sec. old or more
+Get-ChildItem ".\*AzureDevOpsBackup*Build at*" -File | Where-Object CreationTime -lt (Get-Date).AddSeconds(-2) | Remove-Item -Force
 
 #Get file v. for last build ServiceAccounts.exe file
 $FileVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo(".\AzureDevOpsBackup.exe").FileVersion
 
 #Rename file to v. and buildtime
 Get-ChildItem ".\AzureDevOpsBackup.exe" | Where-Object {!$_.PSIsContainer -and $_.extension -eq '.exe'} | Rename-Item -NewName {"$($_.BaseName) v. $FileVersion - Build at $(Get-Date -format "ddMMyyyy-HHmmss")$($_.extension)"} -Force
-
-#Delete old .exe file there not need to be used anymore
-#Get-ChildItem ".\bin\x64" -Recurse -File | Where CreationTime -lt (Get-Date).AddSeconds(-5) | Remove-Item -Force
-#Get-ChildItem ".\*AzureDevOpsBackup*" -File | Where-Object CreationTime -lt (Get-Date).AddSeconds(-5) | Remove-Item -Force
