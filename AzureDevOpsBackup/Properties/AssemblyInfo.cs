81500a00d36a505345919dccf250a17681a4f12d
Michael Morten Sonne <michael@sonnes.cloud>
2022-12-31 12:54:59 +0100

    Delete unused function

--- AzureDevOpsBackup/Properties/AssemblyInfo.cs
+++ AzureDevOpsBackup/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AzureDevOpsBackup")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Azure DevOps Backup - For Git Projects and is using the API for Azure DevOps")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AzureDevOpsBackup")]
