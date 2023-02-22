0a9f02bad9806c4bf21b190e7a63141e4bd11717
Michael Morten Sonne <49366751+michaelmsonne@users.noreply.github.com>
2023-02-22 19:43:54 +0100

    Small changes to functions and and cleanup Change output v. number

--- AzureDevOpsBackup/Properties/AssemblyInfo.cs
+++ AzureDevOpsBackup/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.3.3")]
-[assembly: AssemblyFileVersion("1.0.3.2")]
+[assembly: AssemblyFileVersion("1.0.3.3")]
