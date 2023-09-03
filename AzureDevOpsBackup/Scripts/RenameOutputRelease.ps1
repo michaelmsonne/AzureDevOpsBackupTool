171cd859b196f55284ce6d729cb2df447ff1fa02
Michael Morten Sonne <michael@sonnes.cloud>
2023-09-03 19:29:40 +0200

    Update build event scripts

--- AzureDevOpsBackup/Scripts/RenameOutputRelease.ps1
+++ AzureDevOpsBackup/Scripts/RenameOutputRelease.ps1
@@ -1,3 +1,59 @@
+param (
+    [string]$OutputFolderPath
+)
+
+# Remove any double quotes from the provided $OutputFolderPath parameter
+$OutputFolderPath = $OutputFolderPath -replace '"', ''
+
+# Determine the source folder containing AzureDevOpsBackup.exe
+$SourceFolderPath = (Get-Location).Path
+$AzureDevOpsBackupFile = Get-ChildItem -Path $SourceFolderPath -Filter "AzureDevOpsBackup.exe" -Recurse | Sort-Object CreationTime -Descending | Select-Object -First 1
+
+if ($AzureDevOpsBackupFile) {
+    # Check if the output folder exists, if not, create it
+    if (-not (Test-Path -Path $OutputFolderPath -PathType Container)) {
+        Write-Host "Output folder doesn't exist - Creating it..."
+        New-Item -Path $OutputFolderPath -ItemType Directory
+    }
+
+    # Move files to the output folder, excluding the .\old folder
+    $SearchPattern = "*AzureDevOpsBackup*Build at*.exe"
+    $FilesToMove = Get-ChildItem -Path $SourceFolderPath -Filter $SearchPattern -Recurse | Where-Object { $_.DirectoryName -ne "$OutputFolderPath\old" }
+    foreach ($File in $FilesToMove) {
+        $DestinationPath = Join-Path -Path $OutputFolderPath -ChildPath $File.Name
+        Copy-Item -Path $File.FullName -Destination $DestinationPath -Force
+    }
+
+    # Delete old .exe files that are 2 seconds or more old
+    $OutputFolderPath = $OutputFolderPath.TrimEnd("\")
+    Get-ChildItem -Path $OutputFolderPath -Filter $SearchPattern -File | Where-Object CreationTime -lt (Get-Date).AddSeconds(-2) | Remove-Item -Force
+
+    # Get the version of the AzureDevOpsBackup.exe file
+    $FileVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($AzureDevOpsBackupFile.FullName).FileVersion
+
+    # Rename the file to include version and build time
+    $NewFileName = "{0} v. {1} - Build at {2}{3}" -f $AzureDevOpsBackupFile.BaseName, $FileVersion, (Get-Date -Format "ddMMyyyy-HHmmss"), $AzureDevOpsBackupFile.Extension
+    $NewFilePath = Join-Path -Path $OutputFolderPath -ChildPath $NewFileName
+    Rename-Item -Path $AzureDevOpsBackupFile.FullName -NewName $NewFilePath -Force
+
+    # Copy old files to the .\old folder while keeping the original filenames
+    $OldFolderPath = Join-Path -Path $OutputFolderPath -ChildPath "old"
+    if (-not (Test-Path -Path $OldFolderPath -PathType Container)) {
+        Write-Host "Creating .\old folder..."
+        New-Item -Path $OldFolderPath -ItemType Directory
+    }
+
+    # Copy old files to the .\old folder while keeping the original filenames
+    $OldFiles = Get-ChildItem -Path $OutputFolderPath -Filter $SearchPattern -Recurse | Where-Object { $_.DirectoryName -ne $OldFolderPath }
+    foreach ($OldFile in $OldFiles) {
+        $OldFileDestinationPath = Join-Path -Path $OldFolderPath -ChildPath $OldFile.Name
+        Copy-Item -Path $OldFile.FullName -Destination $OldFileDestinationPath -Force
+    }
+} else {
+    Write-Host "AzureDevOpsBackup.exe not found in the source folder: $SourceFolderPath"
+}
+
+<#
 #Folder for old builds
 $FolderName = ".\Old\"
 if(Get-Item -Path $FolderName -ErrorAction Ignore) {   
@@ -17,3 +73,4 @@ $FileVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo(".\AzureDevO
 
 #Rename file to v. and buildtime - keep original
 Get-ChildItem ".\AzureDevOpsBackup.exe" | Where-Object {!$_.PSIsContainer -and $_.extension -eq '.exe'} | Copy-Item -Path ".\AzureDevOpsBackup.exe" -Destination {"$($_.BaseName) v. $FileVersion - Build at $(Get-Date -format "ddMMyyyy-HHmmss")$($_.extension)"} -Force
+#>
